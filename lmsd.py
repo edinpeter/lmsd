a981e96100d5691aa3778ef458e47d37ee481a38
edinpeter <edin.3@osu.edu>
2018-02-28 15:57:00 -0500

    Seperated dice, standardized sizes

--- lmsd.py
+++ lmsd.py
@@ -9,7 +9,8 @@ import cv2
 import numpy as np
 
 strip = True
-
+square_off = True
+PADDING = 10
 def strip_bg(bg, img):
     c1,c2,c3,c4 = cv2.split(img)
 
@@ -22,6 +23,41 @@ def strip_bg(bg, img):
 
     return cv2.merge((c1, c2, c3, new_alpha))
 
+def crop(img, min_thresh=np.array((0,0,0,255)), max_thresh=np.array((255,255,255,255))):
+    mask = cv2.inRange(img,min_thresh, max_thresh)
+    #mask = np.where(mask == 0, 255, 0)
+    im2, contours,hierarchy = cv2.findContours(mask, 1, 2)
+    #cv2.drawContours(img, contours, -1, (0,255,0), 3)
+
+    cnt = contours[0]
+    perimeter = cv2.arcLength(cnt, True)
+    epsilon = 0.01 * perimeter
+    bb_c = cv2.approxPolyDP(cnt, epsilon, True)
+    min_x, max_x, min_y, max_y = get_rect(bb_c)
+    #cv2.drawContours(img, [bb_c], -1, (0,0,255), 3)
+
+    #cv2.rectangle(img, (min_y, min_x), (max_y, max_x), (0,255,0))
+    max_dim = max(max_x - min_x, max_y - min_y)
+    #print "Max dim: %i" % (max_dim)
+#   cropped_img = img[min_y - PADDING : max_y + PADDING, min_x - PADDING : max_x + PADDING]
+    cropped_img = img[min_x - PADDING : max_x + PADDING, min_y - PADDING : max_y + PADDING]
+    #cv2.imshow("win", cropped_img)
+    #cv2.waitKey(0)
+    cropped_img = cv2.resize(cropped_img, (400,400))
+    return cropped_img
+
+def get_rect(bounding_box):
+    min_x = 99999
+    min_y = 99999
+    max_y = -1
+    max_x = -1
+    for point in bounding_box:
+        #print point
+        min_x = min(min_x, point[0][1])
+        max_x = max(max_x, point[0][1])
+        max_y = max(max_y, point[0][0])
+        min_y = min(min_y, point[0][0])
+    return min_x, max_x, min_y, max_y
 
 
 class Cube(object):
@@ -211,22 +247,15 @@ def main(label, texture):
     cube = Cube(texture)
     #----------- Main Program Loop -------------------------------------
     # --- Main event loop
-    for i in range(0,100): # User did something
+    for i in range(0,700): # User did something
         colors = (random.random(), random.random(), random.random(),)
         glClearColor(colors[0], colors[1], colors[2], 1)
 
-        if i % 3 == 0:
+        for op in [cube.rotate_x, cube.rotate_y, cube.rotate_z]:
             r1 = random.randint(0,300)
             for q in range(r1):
-                cube.rotate_x()
-        elif i % 2 == 0:
-            r1 = random.randint(0,300)
-            for q in range(r1):
-                cube.rotate_y()
-        elif i % 3 == 0:
-            r1 = random.randint(0,300)
-            for q in range(r1):
-                cube.rotate_z()
+                op()
+
         cube.render_scene()
 
         pygame.display.flip()
@@ -236,8 +265,12 @@ def main(label, texture):
             img = cv2.imread('dice/' + str(label) + "_image" + str(i) + ".png", cv2.IMREAD_UNCHANGED)
             img = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
             bg = img[0][0]
-            cv2.imwrite('dice_no_bg/' + str(label) + "_image" + str(i) + ".png", strip_bg(bg, img))
-    
+            cv2.imwrite('dice_no_bg/' + str(label) + "_image" + str(i) + "_nbg.png", strip_bg(bg, img))
+            if square_off:
+                img = cv2.imread('dice_no_bg/' + str(label) + "_image" + str(i) + "_nbg.png", cv2.IMREAD_UNCHANGED)
+                #print img
+                cv2.imwrite('dice_no_bg_squared/' + str(label) + "_image" + str(i) + "_nbg.png", crop(img))
+
     
     cube.delete_texture()
     pygame.quit()
--- strip.py
+++ strip.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 
+PADDING = 10
+
 def strip_bg(bg, img):
 	c1,c2,c3,c4 = cv2.split(img)
 
@@ -14,9 +16,51 @@ def strip_bg(bg, img):
 	print new_alpha, "\n\n", c1
 	return cv2.merge((c1, c2, c3, new_alpha))
 
-img = cv2.imread("gate.png", cv2.IMREAD_UNCHANGED)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
-print img.shape
-bg = (255,255,255)
+def crop(img, min_thresh=np.array((0,0,0,255)), max_thresh=np.array((255,255,255,255))):
+	mask = cv2.inRange(img,min_thresh, max_thresh)
+	#mask = np.where(mask == 0, 255, 0)
+	im2, contours,hierarchy = cv2.findContours(mask, 1, 2)
+	#cv2.drawContours(img, contours, -1, (0,255,0), 3)
 
-cv2.imwrite("gate_trans.png", strip_bg(bg, img))
+	cnt = contours[0]
+	perimeter = cv2.arcLength(cnt, True)
+	epsilon = 0.01 * perimeter
+	bb_c = cv2.approxPolyDP(cnt, epsilon, True)
+	min_x, max_x, min_y, max_y = get_rect(bb_c)
+	cv2.drawContours(img, [bb_c], -1, (0,0,255), 3)
+
+	cv2.rectangle(img, (min_y, min_x), (max_y, max_x), (0,255,0))
+	cv2.imshow("win", img)
+	cv2.waitKey(0)
+
+	max_dim = max(max_x - min_x, max_y - min_y)
+	print "Max dim: %i" % (max_dim)
+#	cropped_img = img[min_y - PADDING : max_y + PADDING, min_x - PADDING : max_x + PADDING]
+	cropped_img = img[min_x - PADDING : max_x + PADDING, min_y - PADDING : max_y + PADDING]
+	
+	cv2.imshow("win", cropped_img)
+	cv2.waitKey(0)
+	
+	cropped_img = cv2.resize(cropped_img, (400,400))
+	return cropped_img
+
+def get_rect(bounding_box):
+	min_x = 99999
+	min_y = 99999
+	max_y = -1
+	max_x = -1
+	for point in bounding_box:
+		print point
+		min_x = min(min_x, point[0][1])
+		max_x = max(max_x, point[0][1])
+		max_y = max(max_y, point[0][0])
+		min_y = min(min_y, point[0][0])
+	return min_x, max_x, min_y, max_y
+
+img = cv2.imread("dice_no_bg/1_image18_nbg.png", cv2.IMREAD_UNCHANGED)
+#img = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
+#print img
+bg = (255,255,255)
+img = crop(img)
+
+cv2.imwrite("cropped_dice/3_image323_nbg.png", img)
